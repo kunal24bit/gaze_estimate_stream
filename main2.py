33443e515764b3fc6c58c16484bebccb2e4673d2
Kunal Verma <64734142+kunal24bit@users.noreply.github.com>
2023-06-20 13:40:29 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -41,7 +41,12 @@ def callback(frame):
 
     return av.VideoFrame.from_ndarray(img, format = "bgr24")
 
-webrtc_streamer(key = "example", video_frame_callback=callback)
+webrtc_streamer(key = "example", 
+                video_frame_callback=callback,
+               rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+        }
+     )
 
 
 # def main():
